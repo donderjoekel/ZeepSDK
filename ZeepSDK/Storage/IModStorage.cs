778e9fa1efb3e262b5aa31455bc2057c36d9ebaf
Christiaan Bloemendaal <github@tnrd.net>
2023-07-10 20:57:12 +0200

    feat: add mod storage

---
+++ ZeepSDK/Storage/IModStorage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ZeepSDK.Storage;
+
+/// <summary>
+/// A storage object that allows you to save and load data to a directory for your mod
+/// </summary>
+public interface IModStorage
+{
+    /// <summary>
+    /// Allows you to add a custom json converter
+    /// </summary>
+    /// <param name="converter">The converter to add</param>
+    void AddConverter(JsonConverter converter);
+
+    /// <summary>
+    /// Allows you to remove a custom json converter
+    /// </summary>
+    /// <param name="converter">The converter to remove</param>
+    void RemoveConverter(JsonConverter converter);
+
+    /// <summary>
+    /// Saves data to a json file
+    /// </summary>
+    /// <param name="name">The name of the file without extension</param>
+    /// <param name="data">The data to save</param>
+    void SaveToJson(string name, object data);
+
+    /// <summary>
+    /// Loads data from a json file
+    /// </summary>
+    /// <param name="name">The name of the file without extension</param>
+    /// <returns></returns>
+    object LoadFromJson(string name);
+
+    /// <summary>
+    /// Loads data from a json file with a specific type
+    /// </summary>
+    /// <param name="name">The name of the file without extension</param>
+    /// <param name="type">The type the data should be</param>
+    /// <returns></returns>
+    object LoadFromJson(string name, Type type);
+
+    /// <summary>
+    /// Loads data from a json file with a specific type
+    /// </summary>
+    /// <param name="name">The name of the file without extension</param>
+    /// <typeparam name="TData">The type the data should be</typeparam>
+    /// <returns></returns>
+    TData LoadFromJson<TData>(string name);
+}
