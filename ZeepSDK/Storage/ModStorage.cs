70993015f93d061a2e229108fe6d3c783d20d72f
Christiaan Bloemendaal <github@tnrd.net>
2023-07-14 14:27:30 +0200

    fix: create directories

--- ZeepSDK/Storage/ModStorage.cs
+++ ZeepSDK/Storage/ModStorage.cs
@@ -58,13 +58,19 @@ internal class ModStorage : IModStorage
             "Zeepkist",
             "Mods",
             plugin.Info.Metadata.GUID);
-
-        Directory.CreateDirectory(directoryPath);
     }
 
     private string CreatePath(string name, string extension = ".json")
     {
-        return Path.Combine(directoryPath, name) + extension;
+        string filePath = Path.Combine(directoryPath, name) + extension;
+
+        string directoryName = Path.GetDirectoryName(filePath);
+        if (!string.IsNullOrEmpty(directoryName) && !Directory.Exists(directoryName))
+        {
+            Directory.CreateDirectory(directoryName);
+        }
+
+        return filePath;
     }
 
     public void AddConverter(JsonConverter converter)
