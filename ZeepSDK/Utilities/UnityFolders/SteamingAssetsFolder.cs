ee38effdd0614c0e4a6b40548bcb6935637fc46b
Christiaan Bloemendaal <github@tnrd.net>
2023-07-23 16:54:20 +0200

    feat: add UnityFolders utilities

---
+++ ZeepSDK/Utilities/UnityFolders/SteamingAssetsFolder.cs
@@ -0,0 +1,74 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace ZeepSDK.UnityFolders;
+
+/// <summary>
+/// An interface for accessing the game's StreamingAssets folder.
+/// </summary>
+[PublicAPI]
+public static class SteamingAssetsFolder
+{
+    private static readonly UnityFolder folder = new UnityFolder(Application.streamingAssetsPath);
+
+    /// <summary>
+    /// Creates a file with the given contents
+    /// </summary>
+    /// <param name="filename">The filename including extension</param>
+    /// <param name="contents">The contents to write</param>
+    public static void CreateFile(string filename, byte[] contents)
+    {
+        folder.CreateFile(filename, contents);
+    }
+
+    /// <summary>
+    /// Creates a file with the given contents
+    /// </summary>
+    /// <param name="filename">The filename including extension</param>
+    /// <param name="contents">The contents to write</param>
+    public static void CreateFile(string filename, string contents)
+    {
+        folder.CreateFile(filename, contents);
+    }
+
+    /// <summary>
+    /// Deletes a file
+    /// </summary>
+    /// <param name="filename">The filename, including extension, to delete</param>
+    public static void DeleteFile(string filename)
+    {
+        folder.DeleteFile(filename);
+    }
+
+    /// <summary>
+    /// Copies a file to the destination
+    /// </summary>
+    /// <param name="sourcePath">The full path of the source file</param>
+    /// <param name="destinationFilename">The destination filename including extension</param>
+    /// <param name="overwrite">If a file with the name already exists, should it be overwritten</param>
+    public static void CopyFile(string sourcePath, string destinationFilename, bool overwrite)
+    {
+        folder.CopyFile(sourcePath, destinationFilename, overwrite);
+    }
+
+    /// <summary>
+    /// Moves a file to the destination
+    /// </summary>
+    /// <param name="sourcePath">The full path of the source file</param>
+    /// <param name="destinationFilename">The destination filename including extension</param>
+    /// <param name="overwrite">If a file with the name already exists, should it be overwritten</param>
+    public static void MoveFile(string sourcePath, string destinationFilename, bool overwrite)
+    {
+        folder.MoveFile(sourcePath, destinationFilename, overwrite);
+    }
+
+    /// <summary>
+    /// Checks if the given file exists
+    /// </summary>
+    /// <param name="filename">The name of the file, including extension</param>
+    /// <returns>True if it exists, false if it doesn't</returns>
+    public static bool Exists(string filename)
+    {
+        return folder.Exists(filename);
+    }
+}
---
+++ ZeepSDK/Utilities/UnityFolders/UnityFolder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace ZeepSDK.UnityFolders;
+
+internal class UnityFolder
+{
+    private readonly string path;
+
+    public UnityFolder(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            throw new ArgumentNullException(path);
+
+        this.path = path;
+    }
+
+    private void EnsureDirectory(string destinationPath)
+    {
+        string directoryName = Path.GetDirectoryName(destinationPath);
+        if (string.IsNullOrEmpty(directoryName))
+            throw new InvalidOperationException("Directory name is null or empty.");
+        if (!Directory.Exists(directoryName))
+            Directory.CreateDirectory(directoryName);
+    }
+
+    public void CreateFile(string filename, byte[] contents)
+    {
+        string destinationPath = Path.Combine(path, filename);
+        EnsureDirectory(destinationPath);
+        File.WriteAllBytes(destinationPath, contents);
+    }
+
+    public void CreateFile(string filename, string contents)
+    {
+        string destinationPath = Path.Combine(path, filename);
+        EnsureDirectory(destinationPath);
+        File.WriteAllText(destinationPath, contents);
+    }
+
+    public void DeleteFile(string filename)
+    {
+        string destinationPath = Path.Combine(path, filename);
+        if (File.Exists(destinationPath))
+            File.Delete(destinationPath);
+    }
+
+    public void CopyFile(string sourcePath, string destinationFilename, bool overwrite)
+    {
+        string destinationPath = Path.Combine(path, destinationFilename);
+        EnsureDirectory(destinationPath);
+        File.Copy(sourcePath, destinationPath, overwrite);
+    }
+
+    public void MoveFile(string sourcePath, string destinationFilename, bool overwrite)
+    {
+        string destinationPath = Path.Combine(path, destinationFilename);
+        EnsureDirectory(destinationPath);
+        if (File.Exists(destinationFilename) && overwrite)
+            File.Delete(destinationFilename);
+        File.Move(sourcePath, destinationPath);
+    }
+
+    public bool Exists(string filename)
+    {
+        return File.Exists(Path.Combine(path, filename));
+    }
+}
