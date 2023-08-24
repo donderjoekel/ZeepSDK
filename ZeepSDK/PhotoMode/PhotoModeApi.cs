1b11ad1d28011d53b1a681330022dcea2f53c636
Christiaan Bloemendaal <github@tnrd.net>
2023-08-24 22:26:51 +0200

    feat: add PhotoModeApi

---
+++ ZeepSDK/PhotoMode/PhotoModeApi.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using ZeepSDK.PhotoMode.Patches;
+
+namespace ZeepSDK.PhotoMode;
+
+/// <summary>
+/// An API for interacting with Photo Mode
+/// </summary>
+[PublicAPI]
+public static class PhotoModeApi
+{
+    /// <summary>
+    /// An event that fires whenever you enter Photo Mode
+    /// </summary>
+    public static event PhotoModeEnteredDelegate PhotoModeEntered;
+
+    /// <summary>
+    /// An event that fires whenever you exit Photo Mode
+    /// </summary>
+    public static event PhotoModeExitedDelegate PhotoModeExited;
+
+    internal static void Initialize()
+    {
+        EnableFlyingCamera2_ToggleFlyingCamera.PhotoModeEntered += () => PhotoModeEntered?.Invoke();
+        EnableFlyingCamera2_ToggleFlyingCamera.PhotoModeExited += () => PhotoModeExited?.Invoke();
+    }
+}
