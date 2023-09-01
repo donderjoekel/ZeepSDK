5c4a2c4badc3b29c8ad2f3a78f160585fedc5cd4
Christiaan Bloemendaal <github@tnrd.net>
2023-09-01 13:42:12 +0200

    feat: add UnsubscribeAsync to WorkshopApi

--- ZeepSDK/Workshop/WorkshopApi.cs
+++ ZeepSDK/Workshop/WorkshopApi.cs
@@ -47,4 +47,31 @@ public static class WorkshopApi
             return Result.Fail(new ExceptionalError(e));
         }
     }
+
+    /// <summary>
+    /// Attempts to unsubscribe from a workshop item
+    /// </summary>
+    /// <param name="workshopId">The id of the workshop item to unsubscribe from</param>
+    /// <returns>Ok if successfully unsubscribed, Fail for multiple failure reasons</returns>
+    public static async UniTask<Result> UnsubscribeAsync(ulong workshopId)
+    {
+        try
+        {
+            Item? item = await Item.GetAsync(workshopId);
+            if (!item.HasValue)
+                return Result.Fail("Unable to get workshop item");
+
+            if (!item.Value.IsSubscribed)
+                return Result.Ok();
+
+            bool subscribed = await item.Value.Unsubscribe();
+
+            return Result.FailIf(!subscribed, "Unable to unsubscribe from workshop item");
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"Unhandled exception in {nameof(UnsubscribeAsync)}: " + e);
+            return Result.Fail(new ExceptionalError(e));
+        }
+    }
 }
