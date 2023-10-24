fff60e63de54b661611324dc6f72a8926b58c04d
Christiaan Bloemendaal <github@tnrd.net>
2023-10-24 21:26:29 +0200

    feat: add PlaylistApi and IPlaylistEditor

---
+++ ZeepSDK/Playlist/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using ZeepSDK.Playlist;
+
+[PublicAPI]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+// ReSharper disable once CheckNamespace
+public static class Extensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+{
+    /// <inheritdoc cref="PlaylistApi.CreateEditor"/>
+    public static IPlaylistEditor CreateEditor(this PlaylistSaveJSON playlist)
+    {
+        return PlaylistApi.CreateEditor(playlist);
+    }
+}
---
+++ ZeepSDK/Playlist/PlaylistEditor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BepInEx.Logging;
+using Newtonsoft.Json;
+using ZeepkistNetworking;
+using ZeepSDK.Utilities;
+
+namespace ZeepSDK.Playlist;
+
+internal class PlaylistEditor : IPlaylistEditor
+{
+    private static readonly ManualLogSource logger = LoggerFactory.GetLogger(typeof(PlaylistEditor));
+
+    private readonly PlaylistSaveJSON playlist;
+
+    public string Name
+    {
+        get => playlist.name;
+        set => playlist.name = value;
+    }
+
+    public bool Shuffle
+    {
+        get => playlist.shufflePlaylist;
+        set => playlist.shufflePlaylist = value;
+    }
+
+    public double RoundLength
+    {
+        get => playlist.roundLength;
+        set => playlist.roundLength = value;
+    }
+
+    public IReadOnlyList<OnlineZeeplevel> Levels => playlist.levels;
+
+    public PlaylistEditor(PlaylistSaveJSON playlist)
+    {
+        this.playlist = playlist;
+    }
+
+    public void AddLevel(string uid, string author, string name, ulong workshopId)
+    {
+        AddLevel(new OnlineZeeplevel
+        {
+            Author = author,
+            UID = uid,
+            Name = name,
+            WorkshopID = workshopId
+        });
+    }
+
+    public void AddLevel(LevelScriptableObject level)
+    {
+        AddLevel(new OnlineZeeplevel
+        {
+            Author = level.Author,
+            Name = level.Name,
+            UID = level.UID,
+            WorkshopID = level.WorkshopID
+        });
+    }
+
+    public void AddLevel(OnlineZeeplevel level)
+    {
+        playlist.levels.Add(level);
+        playlist.amountOfLevels = playlist.levels.Count;
+    }
+
+    public void Save()
+    {
+        string json;
+
+        try
+        {
+            json = JsonConvert.SerializeObject(playlist);
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Failed to serialize playlist: " + e);
+            return;
+        }
+
+        try
+        {
+            string playlistPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Zeepkist",
+                "Playlists",
+                playlist.name + ".zeeplist");
+
+            File.WriteAllText(playlistPath, json);
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Failed to save playlist: " + e);
+        }
+    }
+}
