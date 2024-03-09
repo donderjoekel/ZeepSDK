3c8769a91baa24da40bc0811e84f880ee0b4f363
Christiaan Bloemendaal <github@tnrd.net>
2024-03-09 21:58:30 +0100

    feat: add communication api

---
+++ ZeepSDK/Communication/ComSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using Newtonsoft.Json;
+using ZeepSDK.Utilities;
+
+namespace ZeepSDK.Communication;
+
+internal class ComSender : IComSender
+{
+    private static readonly ManualLogSource logger = LoggerFactory.GetLogger<ComSender>();
+
+    private readonly BaseUnityPlugin plugin;
+
+    public ComSender(BaseUnityPlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    public void SendMessage(string message)
+    {
+        List<IComReceiver> comReceivers = ReceiverRepository.GetReceivers(plugin.Info.Metadata.GUID);
+
+        foreach (IComReceiver comReceiver in comReceivers)
+        {
+            try
+            {
+                if (comReceiver == null)
+                {
+                    logger.LogWarning("Skipping null receiver");
+                    continue;
+                }
+
+                comReceiver.ProcessMessage(message);
+            }
+            catch (Exception e)
+            {
+                logger.LogError("Error while sending message: " + e);
+            }
+        }
+    }
+
+    public void SendAsJson(object obj)
+    {
+        string json = JsonConvert.SerializeObject(obj);
+        SendMessage(json);
+    }
+}
---
+++ ZeepSDK/Communication/ReceiverRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ZeepSDK.Communication;
+
+internal class ReceiverRepository
+{
+    private static readonly Dictionary<string, List<IComReceiver>> receivers = new();
+
+    public static void AddReceiver(IComReceiver receiver)
+    {
+        receivers.TryAdd(receiver.ModIdentifier, new List<IComReceiver>());
+        receivers[receiver.ModIdentifier].Add(receiver);
+    }
+
+    public static void RemoveReceiver(IComReceiver receiver)
+    {
+        if (receivers.TryGetValue(receiver.ModIdentifier, out List<IComReceiver> list))
+            list.Remove(receiver);
+    }
+
+    public static List<IComReceiver> GetReceivers(string modIdentifier)
+    {
+        return receivers.TryGetValue(modIdentifier, out List<IComReceiver> list) ? list : new List<IComReceiver>();
+    }
+}
