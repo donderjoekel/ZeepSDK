d03e84493d3f196b654cf00acfe696f079273734
Christiaan Bloemendaal <github@tnrd.net>
2024-05-24 11:58:59 +0200

    Added extension for getting direct descendants

---
+++ ZeepSDK/Extensions/TransformExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeepSDK.Extensions;
+
+/// <summary>
+/// A class containing extensions for <see cref="Transform"/>
+/// </summary>
+public static class TransformExtensions
+{
+    /// <summary>
+    /// Gets all components of type <typeparamref name="TComponent"/> in the direct descendants of the transform.
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <typeparam name="TComponent"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<TComponent> GetComponentsInDirectDescendants<TComponent>(this Transform transform)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            if (child.TryGetComponent(out TComponent component))
+                yield return component;
+        }
+    }
+}
