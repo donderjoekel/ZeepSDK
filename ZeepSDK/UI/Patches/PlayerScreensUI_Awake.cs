6374c628052a9e351cb0d84917e9e7e73a0979d4
Christiaan Bloemendaal <github@tnrd.net>
2024-05-24 12:01:34 +0200

    Added a bunch of patches

---
+++ ZeepSDK/UI/Patches/PlayerScreensUI_Awake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HarmonyLib;
+
+namespace ZeepSDK.UI.Patches;
+
+[HarmonyPatch(typeof(PlayerScreensUI), nameof(PlayerScreensUI.Awake))]
+internal class PlayerScreensUI_Awake
+{
+    public static event Action<PlayerScreensUI> Awake;
+    
+    private static void Postfix(PlayerScreensUI __instance)
+    {
+        Awake?.Invoke(__instance);
+    }
+}
---
+++ ZeepSDK/UI/Patches/SpectatorCameraUI_Awake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HarmonyLib;
+
+namespace ZeepSDK.UI.Patches;
+
+[HarmonyPatch(typeof(SpectatorCameraUI), nameof(SpectatorCameraUI.Awake))]
+internal class SpectatorCameraUI_Awake
+{
+    public static event Action<SpectatorCameraUI> Awake;
+    
+    private static void Postfix(SpectatorCameraUI __instance)
+    {
+        Awake?.Invoke(__instance);
+    }
+}
