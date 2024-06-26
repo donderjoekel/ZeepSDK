0b8fd697c10ee51c82d3b6b9901cd6dfd4740d79
Christiaan Bloemendaal <github@tnrd.net>
2024-06-26 11:30:04 +0200

    feat: made ComponentCache publicly accessible

--- ZeepSDK/Utilities/ComponentCache.cs
+++ ZeepSDK/Utilities/ComponentCache.cs
@@ -4,10 +4,23 @@ using Object = UnityEngine.Object;
 
 namespace ZeepSDK.Utilities;
 
-internal static class ComponentCache
+/// <summary>
+/// A cache for components that live somewhere in the project.
+/// It uses Object.FindObjectOfType to find the component so this is not particularly efficient.
+/// </summary>
+public static class ComponentCache
 {
-    private static readonly Dictionary<Type, Object> cache = new Dictionary<Type, Object>();
+    private static readonly Dictionary<Type, Object> cache = new();
 
+    /// <summary>
+    /// Returns the component of type T. If it already exists in the cache and is not null it will return that.
+    /// If it does not exist it will try to find it using Object.FindObjectOfType.
+    /// If it finds it, it will cache it and return it.
+    /// If it does not find it, it will return null.
+    /// </summary>
+    /// <param name="includeInactive">Should inactive objects be included</param>
+    /// <typeparam name="T">The type of the component</typeparam>
+    /// <returns></returns>
     public static T Get<T>(bool includeInactive = false) where T : Object
     {
         Type type = typeof(T);
