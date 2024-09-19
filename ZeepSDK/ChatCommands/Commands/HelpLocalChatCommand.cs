bd17e2309b87ba330a693f47854f55e8d6bade8e
Christiaan Bloemendaal <github@tnrd.net>
2024-09-19 13:16:28 +0200

    Added pagination to help command

--- ZeepSDK/ChatCommands/Commands/HelpLocalChatCommand.cs
+++ ZeepSDK/ChatCommands/Commands/HelpLocalChatCommand.cs
@@ -1,4 +1,7 @@
-﻿using ZeepSDK.Chat;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ZeepSDK.Chat;
+using ZeepSDK.Extensions;
 
 namespace ZeepSDK.ChatCommands.Commands;
 
@@ -10,11 +13,34 @@ internal class HelpLocalChatCommand : ILocalChatCommand
 
     public void Handle(string arguments)
     {
+        List<IEnumerable<ILocalChatCommand>> chunks = ChatCommandRegistry.LocalChatCommands.Chunk(5).ToList();
+
         ChatApi.AddLocalMessage("Available commands:");
-        foreach (ILocalChatCommand localChatCommand in ChatCommandRegistry.LocalChatCommands)
+
+        int page = 0;
+
+        if (int.TryParse(arguments, out int parsedPage))
+        {
+            page = parsedPage - 1;
+        }
+
+        if (page >= chunks.Count)
+        {
+            page = chunks.Count - 1;
+        }
+        else if (page < 1)
+        {
+            page = 0;
+        }
+
+        IEnumerable<ILocalChatCommand> chunk = chunks[page];
+
+        foreach (ILocalChatCommand localChatCommand in chunk)
         {
             ChatApi.AddLocalMessage(
                 $"- {localChatCommand.Prefix}{localChatCommand.Command} - {localChatCommand.Description}");
         }
+
+        ChatApi.AddLocalMessage($"Page {page + 1}/{chunks.Count}");
     }
 }
---
+++ ZeepSDK/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace ZeepSDK.Extensions;
+
+/// <summary>
+/// Extensions for lists
+/// </summary>
+public static class ListExtensions
+{
+    /// <summary>
+    /// Chunks a list into chunks of the given size
+    /// </summary>
+    [PublicAPI]
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int size)
+    {
+        List<T> sourceList = source as List<T> ?? source.ToList();
+        for (int index = 0; index < sourceList.Count; index += size)
+        {
+            yield return sourceList.Skip(index).Take(size);
+        }
+    }
+}
