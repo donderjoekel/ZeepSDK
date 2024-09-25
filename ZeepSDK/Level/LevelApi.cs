5f4533c7c7dbca2d4e04272b902de262e4c0472e
Christiaan Bloemendaal <github@tnrd.net>
2024-09-25 20:23:39 +0200

    fix: hardcoded hash for levle 05

--- ZeepSDK/Level/LevelApi.cs
+++ ZeepSDK/Level/LevelApi.cs
@@ -17,6 +17,8 @@ namespace ZeepSDK.Level;
 [PublicAPI]
 public static class LevelApi
 {
+    private const string OldLevel05Hash = "56B1927C4A6BD753558C431D617ECCD68BCF4A39";
+    private const string NewLevel05Hash = "2F6551106C2A82137CF7EE39FB71757DA38866A2";
     private const int PresentBlockID = 2264;
     private static readonly ManualLogSource logger = LoggerFactory.GetLogger(typeof(LevelApi));
     private static readonly Vector3Comparer _vector3Comparer = new();
@@ -123,6 +125,9 @@ public static class LevelApi
             hashBuilder.Append(b.ToString("X2"));
         }
 
-        return hashBuilder.ToString();
+        string levelHash = hashBuilder.ToString();
+        return string.Equals(levelHash, OldLevel05Hash, StringComparison.OrdinalIgnoreCase)
+            ? NewLevel05Hash
+            : levelHash;
     }
 }
