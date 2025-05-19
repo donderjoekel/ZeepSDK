eb9c3cfaf5b021a54210e8a5f4e816b3c87e56c2
Christiaan Bloemendaal <github@tnrd.net>
2025-05-19 08:04:12 +0200

    Added extensions for easy notify

---
+++ ZeepSDK/Extensions/ResultExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using ZeepSDK.Crashlytics;
+using ZeepSDK.External.FluentResults;
+
+namespace ZeepSDK.Extensions;
+
+/// <summary>
+/// Contains extensions for Result
+/// </summary>
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Notifies all ExceptionalErrors to Bugsnag
+    /// </summary>
+    /// <param name="result"></param>
+    public static void NotifyErrors(this ResultBase result)
+    {
+        foreach (ExceptionalError error in result.Errors.Where(x => x is ExceptionalError).Cast<ExceptionalError>())
+        {
+            CrashlyticsApi.Notify(error.Exception);
+        }
+    }
+}
