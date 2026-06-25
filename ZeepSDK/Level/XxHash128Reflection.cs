5a35643baf5b1ada2634c16ff1c64d5cff127a49
James Harris <3440094+wopian@users.noreply.github.com>
2026-06-25 21:39:23 +0100

    fix: cache xxh128 hash delegate

--- ZeepSDK/Level/XxHash128Reflection.cs
+++ ZeepSDK/Level/XxHash128Reflection.cs
@@ -5,19 +5,19 @@ namespace ZeepSDK.Level;
 
 internal static class XxHash128Reflection
 {
-    private static readonly MethodInfo HashMethod = ResolveHashMethod();
+    private static readonly Func<byte[], byte[]> HashFunction = ResolveHashFunction();
 
     internal static byte[] Hash(byte[] bytes)
     {
-        return (byte[])HashMethod.Invoke(null, new object[] { bytes });
+        return HashFunction(bytes);
     }
 
-    private static MethodInfo ResolveHashMethod()
+    private static Func<byte[], byte[]> ResolveHashFunction()
     {
         Type xxHash128Type = Type.GetType("System.IO.Hashing.XxHash128, System.IO.Hashing", throwOnError: true);
         MethodInfo hashMethod = xxHash128Type.GetMethod("Hash", new[] { typeof(byte[]) });
         if (hashMethod == null)
             throw new MissingMethodException("System.IO.Hashing.XxHash128", "Hash(byte[])");
-        return hashMethod;
+        return (Func<byte[], byte[]>)Delegate.CreateDelegate(typeof(Func<byte[], byte[]>), hashMethod);
     }
 }
